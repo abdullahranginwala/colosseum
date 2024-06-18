53aab57eca7b1724ae3b4774fdbee84f9d6b5acd
Abdullah Ranginwala <abdullahranginwala20@gmail.com>
2024-06-18 14:15:35 +0530

    Initial commit

---
+++ src/test/kotlin/com/abdullahranginwala/colosseum/ColosseumApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.abdullahranginwala.colosseum
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ColosseumApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
